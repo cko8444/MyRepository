d9b732e183f8d8e556649fcf741e2ca0e069e939
cko8444 <87625423+cko8444@users.noreply.github.com>
2024-10-24 17:41:58 -0400

    Update BallotV2.sol

--- BallotV1.sol
+++ BallotV1.sol
@@ -15,6 +15,24 @@ contract BallotV1 {
     mapping(address => Voter) voters;
     Proposal[] proposals;
 
-    enum Phase {Init, Regs, Vote, Done}
-    Phase public state = Phase.Init;
+	enum Phase {Init, Regs, Vote, Done}
+
+	Phase public state = Phase.Init;
+
+	constructor (uint numProposals) public  {
+		chairPerson = msg.sender;
+		voters[chairPerson].weight = 2;
+		for (uint prop = 0; prop < numProposals; prop++)
+			proposals.push(Proposal(0)); 
+	}
+
+	// function for changing Phase; can be done only by chairPerson
+	function changeState(Phase ) public {
+		if (msg.sender != chairPerson) { revert(); }
+
+		if (x < state) revert();
+
+		state = x;
+	}
+		
 }
